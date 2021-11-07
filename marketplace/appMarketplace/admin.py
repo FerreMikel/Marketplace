c424103a9117a3afdb826f71d434b641b2afc3ab
Mikel Ferrer <ferremikel@gmail.com>
2021-11-07 15:10:44 +0100

    Models created, Inicio view created
    Co-authored-by: jonleinena <jonleinena@users.noreply.github.com>
    Co-authored-by: Álvaro Gutiérrez <alvarogugon@gmail.com>

--- marketplace/appMarketplace/admin.py
+++ marketplace/appMarketplace/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Categoria, Caracteristica, Fabricante, Producto, Imagen
+
+admin.site.register(Categoria)
+admin.site.register(Caracteristica)
+admin.site.register(Fabricante)
+admin.site.register(Producto)
+admin.site.register(Imagen)
--- marketplace/appMarketplace/models.py
+++ marketplace/appMarketplace/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. wenas tardes
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Fabricante(models.Model):
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=500)
+    sitioWeb = models.URLField()
+    logoURL = models.URLField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Caracteristica(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=60)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    fabricante = models.ForeignKey(Fabricante, on_delete=models.CASCADE)
+    precio = models.IntegerField()
+    caracteristicas = models.ManyToManyField(Caracteristica)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Imagen(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.producto
